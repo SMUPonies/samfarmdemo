283cc1f3ad75955c3d2e44a88694fe04738553f5
SMUPonies <andrew_dennis@att.net>
2017-01-09 10:08:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ exports.handler = (event, context, callback) => {
 
     emitLambdaAge();
 
-    var samCount = 8;
+    var samCount = 20;
     var samMultiplier = process.env.SAM_MULTIPLIER;
     var totalSAMs = samCount * samMultiplier;
 
